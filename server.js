397a759500824051369912bf78ca88b5d0a73220
Matt Sweeney <msweeney777@gmail.com>
2018-12-20 20:36:31 -0500

    Minor cleanup in server.js.

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 
-var app = express();
+const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
-
-app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
